6d1a3468f2f7400477a43e966605747c6997a0d7
Sankhat Shivbhadrasinh <112846791+Shivrajbanna609@users.noreply.github.com>
2025-01-10 16:43:32 +0530

    reset

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,12 @@ import Login from './pages/Login/Login.js'
 import { ToastContainer } from 'react-toastify';
 import ForgetPassword from './pages/ForgetPassword/ForgetPassword.js';
 import AdminPage from './pages/AdminPage/AdminPage.js';
-import Favourite from './pages/FavouritePage/Favourite.js';
 function App() {
   return (
     <> 
       <ToastContainer/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/favourite' element={<Favourite/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/petpage' element={<PetPage/>}/>
         <Route path='/forget-password' element={<ForgetPassword/>}/>
